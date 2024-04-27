52fba3a5ab53a89fbbbae2976e20c7e988cff79d
Jaret Varn <jav76@uakron.edu>
2024-04-27 08:45:06 -0400

    Init data access project

---
+++ MTGDataAccess/DataAccess.cs
@@ -0,0 +1,10 @@
+﻿namespace MTGDataAccess
+{
+    /// <summary>
+    /// This project should be used for all raw data access. This should include API calls, file storage, database access, etc.
+    /// </summary>
+    public class DataAccess
+    {
+
+    }
+}
